b2522124d506096de6932886ab3de4193c6e2b01
dinesh dewangan <d0d02tp@homeoffice.wal-mart.com>
2021-06-07 13:42:11 +0530

    updated some files

--- src/main/java/com/dkdew/chaos/monkey/filter/AppController.java
+++ src/main/java/com/dkdew/chaos/monkey/filter/AppController.java
@@ -1,4 +1,4 @@
-package org.zerotul.chaos.monkey.filter;
+package com.dkdew.chaos.monkey.filter;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/dkdew/chaos/monkey/filter/Application.java
+++ src/main/java/com/dkdew/chaos/monkey/filter/Application.java
@@ -1,4 +1,4 @@
-package org.zerotul.chaos.monkey.filter;
+package com.dkdew.chaos.monkey.filter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
